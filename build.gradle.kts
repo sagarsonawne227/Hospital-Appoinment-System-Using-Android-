c5ba0645e880d3edc0be4e2c2500b8bd934ea74d
Sagar Sonawane <111235065+sagarsonawne227@users.noreply.github.com>
2025-04-03 23:36:05 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
